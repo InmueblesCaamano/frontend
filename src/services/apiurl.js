97b9a7c1186659e2ca0443f617f4d6a309cb2ce5
manuel perez <manuelperez.0000@gmail.com>
2024-01-05 19:30:11 -0400

    qwe

--- src/services/apiurl.js
+++ src/services/apiurl.js
@@ -3,4 +3,5 @@
 //https://backendcaamano.vercel.app/api/v1
 
 // eslint-disable-next-line no-undef
-export const ApiUrl =  process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api/v1' : 'https://backendcaamano.vercel.app/api/v1'
+export const ApiUrl =  process.env.NODE_ENV === 'development' ? 'https://backendcaamano.vercel.app/api/v1' : 'https://backendcaamano.vercel.app/api/v1'
+//export const ApiUrl =  process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api/v1' : 'https://backendcaamano.vercel.app/api/v1'
