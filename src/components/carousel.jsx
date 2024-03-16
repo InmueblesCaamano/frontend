e5865f0b577b943fb798da5b828a8ac5b18d5a35
manuel perez <manuelperez.0000@gmail.com>
2024-03-16 13:09:43 -0400

    roraima

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,47 +1,8 @@
 export default function Carousel() {
     return (<div className='background-img'>
-        <div className='land'>
-            <div className='imgWrapper'>
-                <img src='/img/logoTransparent.png' alt='logo' className="" width={'100%'} />
-            </div>
-            {/* <div className="container bg-light p-4 wrapFinder">
-                <div className="row">
-                    <div className="col-12 col-lg-2">
-                        <div className="text-start">
-                            Venta o alquiler
-                        </div>
-                        <select className="form-select" name="" id="">
-                            <option value=""> En Venta </option>
-                            <option value=""> En Alquiler </option>
-                        </select>
-                    </div>
-                    <div className="col-12 col-lg-4">
-                        <div className="text-start">
-                            Ubicacion (Nueva Esparta)
-                        </div>
-                        <select className="form-select" name="" id="">
-                            <option value=""> Antonio Diaz </option>
-                            <option value=""> Mariño </option>
-                            <option value=""> Maneiro </option>
-                        </select>
-                    </div>
-                    <div className="col-12 col-lg-4">
-                        <div className="text-start">
-                            Tipo
-                        </div>
-                        <select className="form-select" name="" id="">
-                            <option value=""> Casa </option>
-                            <option value=""> Apartamento </option>
-                            <option value=""> TomHouse </option>
-                            <option value=""> Terrenos </option>
-                            <option value=""> Locales Comerciales </option>
-                        </select>
-                    </div>
-                    <div className="col-12 col-lg-2 btn-buscar-wrap">
-                        <button className="btn btn-primary px-5"> <i className='px-4 bi bi-search' /> Buscar </button>
-                    </div>
-                </div>
-            </div> */}
+        <div className='imgWrapper'>
+            <img src='/img/logoTransparent.png' alt='logo' width={350} />
         </div>
-    </div>)
+    </div>
+    )
 }
--- src/components/whatsappBotton.jsx
+++ src/components/whatsappBotton.jsx
@@ -1,7 +1,7 @@
 import ws from '/img/logo-ws.svg';
 const WhatsappButton = () => {
     return (
-        <a href='#' onClick={() => window.open('https://wa.me/+584248261631', '_blank')} className='ws-button'>
+        <a href='#' onClick={() => window.open('https://wa.me/+584142110527', '_blank')} className='ws-button'>
             <img className='ws-img-btn' src={ws} width={50} height={50} alt='ws' />
         </a>
     )
